fe06d957055a4467e9d692c0947a0aad0928634c
Han Li <hl565@cornell.edu>
2018-03-10 18:09:34 -0500

    create db schema, migrations, fixing prepopulate

--- backend/app/models.py
+++ backend/app/models.py
@@ -4,8 +4,31 @@ from __future__ import unicode_literals
 from django.db import models
 import uuid
 
-
-# where to create models for db
 class Company(models.Model):
     companyid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     companyname = models.CharField(max_length=200)
+
+class Image(models.Model):
+    imageid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    imagefield = models.ImageField()
+    
+class Station(models.Model):
+    stationid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    companyid = models.ForeignKey(
+        'Company',
+        on_delete=models.CASCADE)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+
+class Upload(models.Model):
+    uploadid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    timestamp = models.DateField(auto_now_add=True)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    stationid = models.ForeignKey(
+        'Station',
+        on_delete=models.CASCADE)
+    price = models.CharField(max_length=10)
+    imageid = models.ForeignKey(
+        'Image',
+        on_delete=models.CASCADE)
--- backend/prepopulate.py
+++ backend/prepopulate.py
@@ -1,22 +1,41 @@
 import os
 import sys
 import json
+from django.conf import settings
 
-def populate_database(data_files):
+def populate_database():
     # delete all rows
     Company.objects.all().delete()
-    Company.objects.get_or_create(
-        companyid="1",
+    Image.objects.all().delete()
+    Station.objects.all().delete()
+    Upload.objects.all().delete()
+
+    company, _ = Company.objects.get_or_create(
         companyname="exxon"
     )
+    sample_image_path = os.path.join(settings.STATIC_ROOT, 'prepopulate_sign.jpg')
+    image, _ = Image.imagefield(sample_image_path, f.read())
+    station, _ = Station.objects.get_or_create(
+        companyid = company.companyid,
+        latitude = 14.10618,
+        longitude = -44.78551
+    )
+    Upload.objects.get_or_create(
+        latitude = -27.83406,
+        longitude = 137.13269,
+        stationid = station.stationid,  
+        price = "1.99",
+        imageid = image.imageid
+    )
+
 
 # Start execution here!
 if __name__ == '__main__':
     reload(sys)
     sys.setdefaultencoding("utf-8")
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'website.local_settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.local_settings')
 
     import django
     django.setup()
-    from app.models import *
+    from app.models import Company, Image, Station, Upload
     populate_database()
