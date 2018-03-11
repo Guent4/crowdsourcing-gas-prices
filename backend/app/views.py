18725260170a6ddd5979972869ff84fdd1aae409
Han Li <hl565@cornell.edu>
2018-03-11 00:00:32 -0500

    implement company mapping endpoint

--- backend/app/views.py
+++ backend/app/views.py
@@ -4,6 +4,8 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import JsonResponse
 
+from models import Company, Image, Station, Upload
+
 def index(request):
     return JsonResponse({"message": "This is just the index page"})
 
@@ -11,7 +13,15 @@ def map(request):
     return JsonResponse({"message": "n"})
 
 def company_mapping(request):
-    return JsonResponse({"message": "c"})
+    companies = Company.objects.all()
+    resp = []
+    for c in companies:
+        comp_dict = {}
+        comp_dict['companyid'] = c.companyid
+        comp_dict['companyname'] = c.companyname
+        resp.append(comp_dict)
+
+    return JsonResponse(resp, safe=False)
 
 def edge_update(request):
     return JsonResponse({"message": "e"})
